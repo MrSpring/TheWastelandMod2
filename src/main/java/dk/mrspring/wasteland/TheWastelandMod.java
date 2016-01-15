f1ad760461c27eec264cb99a5a83edbdb3929880
MrSpring <mrspring@mrspring.dk>
2016-01-15 19:07:57 +0100

    Updated build file

--- src/main/java/dk/mrspring/wasteland/TheWastelandMod.java
+++ src/main/java/dk/mrspring/wasteland/TheWastelandMod.java
@@ -1,8 +1,11 @@
 package dk.mrspring.wasteland;
 
+import net.minecraftforge.fml.common.Mod;
+
 /**
  * Created on 15-01-2016 for TheWastelandMod2.
  */
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.NAME, version = ModInfo.VERSION)
 public class TheWastelandMod
 {
 }
