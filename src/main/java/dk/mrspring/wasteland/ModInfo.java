1e0eb5a613438c78878aafe19a385bc226374203
MrSpring <mrspring@mrspring.dk>
2016-01-15 18:51:07 +0100

    Initial commit

---
+++ src/main/java/dk/mrspring/wasteland/ModInfo.java
@@ -0,0 +1,11 @@
+package dk.mrspring.wasteland;
+
+/**
+ * Created on 15-01-2016 for TheWastelandMod2.
+ */
+public class ModInfo
+{
+    public static final String MOD_ID = "wlm";
+    public static final String NAME = "The Wasteland Mod II";
+    public static final String VERSION = "2.0.0-BETA";
+}
---
+++ src/main/java/dk/mrspring/wasteland/TheWastelandMod.java
@@ -0,0 +1,8 @@
+package dk.mrspring.wasteland;
+
+/**
+ * Created on 15-01-2016 for TheWastelandMod2.
+ */
+public class TheWastelandMod
+{
+}
